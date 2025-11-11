1a01c6db99c1fddcc72f7c245c34c9929e796c56
Ana Carolina Lamann <167484009+Aninha-Lamann@users.noreply.github.com>
2025-11-11 15:48:43 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,61 +1,50 @@
-//Código feito com auxilio do professor no dia 16/09
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+int main()
+{
+	int magic = 0;
+	int guess;
+	int opcao = 0;  // inicializa opcao
+	int contador = 0;
 
-    int magic = 0;
-    int guess;
-    int opcao;
-    int sorteado = 0;
-    
-    srand(time(NULL));
+	srand(time(NULL));  // inicializa gerador de nC:meros aleatC3rios
 
-    do {
-        printf("\n***** JOGO DE ADIVINHACAO *****\n\n");
-        printf("1 - Sortear um novo numero\n");
-        printf("2 - Digitar adivinhacao\n");
-        printf("3 - Mostrar o segredo\n");
-        printf("4 - Sair\n\n");
-        printf("As opcoes validas sao 1, 2, 3 ou 4.\n");
-        printf("Digite sua opcao: ");
-        scanf("%d", &opcao);
-
-        switch (opcao) {
-            case 1:
-                magic = rand() % 100; 
-                sorteado = 1;
-                printf("Numero sorteado com sucesso!\n");
-                break;
-            case 2:
-                if (sorteado == 0) {
-                    printf("Primeiro sorteie um numero!\n");
-                } else {
-                    printf("Digite seu palpite: ");
-                    scanf("%d", &guess);
-                    if (guess == magic)
-                        printf("Parabens! Voce acertou!\n");
-                    else if (guess < magic)
-                        printf("Muito baixo! Tente novamente.\n");
-                    else
-                        printf("Muito alto! Tente novamente.\n");
-                }
-                break;
-            case 3:
-                if (sorteado)
-                    printf("O numero secreto e: %d\n", magic);
-                else
-                    printf("Nenhum numero foi sorteado ainda!\n");
-                break;
-            case 4:
-                printf("Saindo do jogo...\n");
-                break;
-            default:
-                printf("Opcao invalida! Tente novamente.\n");
-                break;
-        }
-    } while (opcao != 4);
-    return 0;
+	while (opcao != 4) {
+		printf("\n***** JOGO DE ADIVINHACAO****\n\n");
+		printf("1 - Sortear um novo numero\n\n");
+		printf("2 - Digitar adivinhacao\n\n");
+		printf("3 - Mostrar o segredo\n\n");
+		printf("4 - sair\n\n");
+		printf("As opcoes validas sao 1 ou 2 ou 3 ou 4\n");
+		printf("Digite sua opcao: ");
+		scanf("%d",&opcao);
+		switch (opcao) {
+		case 1:
+			magic = rand() % 100 + 1;  // nC:mero entre 1 e 100
+			printf("Foi sorteado um novo numero.\n");
+			break;
+		case 2:
+			printf("Digite sua adivinhacao: ");
+			scanf("%d",&guess);
+			if(guess == magic) {
+				printf("* * * PARABENS, ACERTOU! * * *\n");
+			} else {
+				printf("ERROU, vou contar mais um erro!\n");
+				contador = contador +1;
+			}
+			break;
+		case 3:
+			printf("O segredo e: %d\n",magic);
+			break;
+		case 4:
+			printf("Vou encerrar o programa\n");
+			printf("Voce errou %d vezes.\n",contador);
+			break;
+		default:
+			printf("As opcoes validas sao 1 ou 2 ou 3 ou 4\n");
+		}
+	}
+	return 0;
 }
-
