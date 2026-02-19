be5042b51bcbcb280e98885e8a9ceda1140a9bf7
Ana Carolina Lamann <167484009+Aninha-Lamann@users.noreply.github.com>
2026-02-19 15:43:57 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,28 +1,55 @@
 #include <stdio.h>
-
-int main()
-{
-	int A[5];
-	int temp;
-
-	printf("Digite 5 numeros:\n");
-	for(int i = 0; i < 5; i++) {
-		scanf("%d", &A[i]);
-	}
-	for(int i = 0; i < 4; i++) {
-		for(int j = 0; j < 4 - i; j++) {
-			if(A[j] > A[j+1]) {
-				temp = A[j+1];
-				A[j+1] = A[j];
-				A[j] = temp;
-			}
-		}
-	}
-	printf("Numeros ordenados: ");
-	for(int i = 0; i < 5; i++) {
-		printf("%d ", A[i]);
-	}
-	printf("\n");
-
-	return 0;
+ 
+// Função para ler um número inteiro positivo
+int lerNumeroPositivo(const char* mensagem) {
+    int numero;
+    do {
+        printf("%s", mensagem);
+        scanf("%d", &numero);
+        if (numero < 0) {
+            printf("Por favor, insira um número positivo.\n");
+        }
+    } while (numero < 0);
+    return numero;
 }
+ 
+int main() {
+    int total, masc, fem;
+    int falecidas, falecidas_masc, falecidas_fem;
+    float perc_falecidas;
+ 
+    // Ler o número total de crianças nascidas
+    total = lerNumeroPositivo("Digite o numero total de criancas nascidas em 2019: ");
+ 
+    // Ler o número de crianças do sexo masculino
+    masc = lerNumeroPositivo("Digite o numero de criancas do sexo masculino: ");
+   
+    // Calcular o número de crianças do sexo feminino
+    fem = total - masc;
+    printf("Numero de criancas do sexo feminino: %d\n", fem);
+ 
+    // Ler o número de crianças falecidas até 12 meses
+    falecidas = lerNumeroPositivo("Digite o numero de criancas falecidas ate 12 meses: ");
+ 
+    // Validação da quantidade de falecidos masculinos
+    do {
+        falecidas_masc = lerNumeroPositivo("Digite o numero de criancas falecidas do sexo masculino: ");
+        if (falecidas_masc > falecidas || falecidas_masc > masc) {
+            printf("Valor invalido! Deve ser menor ou igual ao numero de falecidas e ao total de meninos.\n");
+        }
+    } while (falecidas_masc > falecidas || falecidas_masc > masc);
+ 
+    // Calcular o número de falecidas do sexo feminino
+    falecidas_fem = falecidas - falecidas_masc;
+ 
+    // Calcular o percentual de crianças falecidas
+    perc_falecidas = ((float) falecidas / total) * 100.0;
+ 
+    // Exibir os resultados
+    printf("\n--- RESULTADOS ---\n");
+    printf("Percentual de criancas falecidas ate 12 meses: %.2f%%\n", perc_falecidas);
+    printf("Quantidade de meninas falecidas ate 12 meses: %d\n", falecidas_fem);
+ 
+    return 0;
+}
+ 
